85354b486bee11d8df6d53911dba6bd068d77573
Nathaniel Miller <dukeoflaser@gmail.com>
2016-01-01 01:29:23 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,12 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+  numbers = (3...limit).to_a
+  numbers.find_all { |n| n % 3 == 0 || n % 5 == 0 }
+end
+
+
+def sum_multiples(limit)
+  multiples = collect_multiples(limit)
+  multiples.inject { |sum, n| sum + n }
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,25 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+  def initialize(limit)
+    unless limit.is_a?(Fixnum)
+      raise ArgumentError.new("Only numbers are allowed")
+    end
+    
+    @limit = limit
+  end
+  
+  def collect_multiples
+    numbers = (3...@limit).to_a
+    numbers.find_all { |n| n % 3 == 0 || n % 5 == 0 }
+  end
+
+  def sum_multiples
+    collect_multiples.inject { |sum, n| sum + n }
+  end
+  
+end
+
+
+
+
